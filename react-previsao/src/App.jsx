0586dc032dd9b28b107d98009dfdf3e8e767f169
kaqueDM1 <kaiquedemetriobrz@gmail.com>
2024-10-08 20:32:20 -0300

    está quase pronto falta a previsão dos 5 dias

--- react-previsao/src/App.jsx
+++ react-previsao/src/App.jsx
@@ -5,7 +5,7 @@ import WeatherInfo from './components/Weather/WeatherInfo'
 
 
 function App() {
-  const [weather, setWeather] = useState({})
+  const[weather, setWeather] = useState()
   const inputRef = useRef()
   
 
@@ -13,23 +13,24 @@ function App() {
     const city = inputRef.current.value
     const key = "9d93dfe4d3c9bcc108aa7f7c7afc6cf3"
     const url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
-  
+    const url5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`
+
     const apiInfo = await axios.get(url)
     setWeather(apiInfo.data)
     
   }
 
   return (
-    
-      <div>
+      <>    
+      <div className='container'>
         <h1>Previsão do Tempo</h1>
         <input ref={inputRef} type="text" placeholder='Digite o nome da cidade' />
         <button onClick={searchCity}>Buscar</button>
 
-        <WeatherInfo weather={weather}/>
+        {weather && <WeatherInfo weather={weather}/>}
+        
       </div>
-      
-    
+      </>
   )
 }
 
--- react-previsao/src/components/Weather/WeatherInfo.jsx
+++ react-previsao/src/components/Weather/WeatherInfo.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable react/prop-types */
-function WeatherInfo ({ weather }){
-        console.log(weather)
-    return(
-        <div>
+
+import './WeatherInfo.css'
+
+function WeatherInfo({ weather }) {
+    console.log(weather)
+    return (
+        <div className='weather-container'>
             <h2>{weather.name}</h2>
-            <div>
+            <div className=' weather-info'>
                 <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} />
-                    <p>{Math.round(weather.main.temp)}°C</p>
-                </div>
-                <p>{weather.weather[0].description}</p>
-                <div>
-                    <p>Sensação térmica:{Math.round(weather.main.feels_like)}°C</p>
-                    <p>Umidade:{weather.main.humidity}%</p>
-                    <p>Pressão:{weather.main.pressure}</p>
+                <p className='temperature'>{Math.round(weather.main.temp)}°C</p>
+            </div>
+            <p className='description'>{weather.weather[0].description}</p>
+            <div className='details'>
+                <p>Sensação térmica: {Math.round(weather.main.feels_like)}°C</p>
+                <p>Umidade: {weather.main.humidity}%</p>
+                <p>Pressão: {weather.main.pressure}</p>
             </div>
         </div>
     )
